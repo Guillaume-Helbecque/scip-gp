0eeaca35024f4a4f699a3ef6df473a0e2582b246
Guillaume-Helbecque <helbecque.guillaume@gmail.com>
2025-06-20 17:56:21 +0200

    turn presolve and heuristics off, and DFS on

--- main.py
+++ main.py
@@ -5,15 +5,22 @@ from instances.generate_instances import generate_instance
 from model.generate_model import create_model
 from util import print_results
 
-from pyscipopt import Model
+from pyscipopt import Model, SCIP_PARAMSETTING
 
 n = 5000
 type = 9
 r = 100000
 id = 17
 
+param_dict = {
+    "nodeselection/dfs/stdpriority": 1073741823,
+}
+
 filename = generate_instance(n, type, r, id)
 scip = create_model(filename)
+scip.setParams(param_dict)
+scip.setHeuristics(SCIP_PARAMSETTING.OFF)
+scip.setPresolve(SCIP_PARAMSETTING.OFF)
 scip.hideOutput()
 scip.optimize()
 print_results(scip)
