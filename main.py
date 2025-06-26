c0c6f344450bd1b76925298d350df7d8488924d9
Guillaume-Helbecque <helbecque.guillaume@gmail.com>
2025-06-26 14:05:10 +0200

    add time limit and plug custom branching rule

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 os.chdir("/home/ghelbecq/Bureau/scip-rl/")
 
 from instances.generate_instances import generate_instance, clean_files
+from branching.StrongBranchingRule import StrongBranchingRule
 from model.generate_model import create_model
 from util import print_results, store_results
 
@@ -14,6 +15,7 @@ from pyscipopt import Model, SCIP_PARAMSETTING
 
 save_output = False
 solve_all   = False
+branch_rule = "customStrongBranching"
 
 n = 100
 type = 11
@@ -21,11 +23,14 @@ r = 1000
 S = 100
 id = 1
 
+output_filename = "test_output.txt"
+
 ## SCIP solving
 
 param_dict = {
     "nodeselection/dfs/stdpriority": 1073741823,
-    "branching/fullstrong/priority": 536870911,
+    # "branching/fullstrong/priority": 536870911,
+    "limits/time": 60,
 }
 
 if solve_all:
@@ -37,10 +42,15 @@ if solve_all:
         scip.setHeuristics(SCIP_PARAMSETTING.OFF)
         scip.setPresolve(SCIP_PARAMSETTING.OFF)
         scip.hideOutput()
+
+        custom_strong_branching = StrongBranchingRule(scip)
+        scip.includeBranchrule(custom_strong_branching, "", "",
+            priority=10000000, maxdepth=-1, maxbounddist=1)
+
         scip.optimize()
         print_results(instancename, scip)
         if save_output:
-            store_results(instancename, scip, "test_output.txt")
+            store_results(instancename, scip, output_filename)
 else:
     # Solve only instance `id`
     instancename = generate_instance(n, type, r, id, S)
@@ -49,9 +59,14 @@ else:
     scip.setHeuristics(SCIP_PARAMSETTING.OFF)
     scip.setPresolve(SCIP_PARAMSETTING.OFF)
     scip.hideOutput()
+
+    custom_strong_branching = StrongBranchingRule(scip)
+    scip.includeBranchrule(custom_strong_branching, "", "",
+        priority=10000000, maxdepth=-1, maxbounddist=1)
+
     scip.optimize()
     print_results(instancename, scip)
     if save_output:
-        store_results(instancename, scip, "test_output.txt")
+        store_results(instancename, scip, output_filename)
 
 clean_files()
