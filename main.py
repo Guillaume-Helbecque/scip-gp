0aaef693124d42527868adaf0923f84d2539ae0d
Guillaume-Helbecque <helbecque.guillaume@gmail.com>
2025-06-26 13:46:53 +0200

    add a solve_all option and update parameters

--- main.py
+++ main.py
@@ -12,20 +12,37 @@ from pyscipopt import Model, SCIP_PARAMSETTING
 
 ## Global parameters
 
-save_output = True
+save_output = False
+solve_all   = False
+
+n = 100
+type = 11
+r = 1000
+S = 100
+id = 1
 
 ## SCIP solving
 
 param_dict = {
     "nodeselection/dfs/stdpriority": 1073741823,
+    "branching/fullstrong/priority": 536870911,
 }
 
-n = 5000
-type = 9
-r = 100000
-S = 10
-
-for id in range(1, S+1):
+if solve_all:
+    # Solve all instances in series (`S` in total)
+    for id in range(1, S+1):
+        instancename = generate_instance(n, type, r, id, S)
+        scip = create_model(instancename)
+        scip.setParams(param_dict)
+        scip.setHeuristics(SCIP_PARAMSETTING.OFF)
+        scip.setPresolve(SCIP_PARAMSETTING.OFF)
+        scip.hideOutput()
+        scip.optimize()
+        print_results(instancename, scip)
+        if save_output:
+            store_results(instancename, scip, "test_output.txt")
+else:
+    # Solve only instance `id`
     instancename = generate_instance(n, type, r, id, S)
     scip = create_model(instancename)
     scip.setParams(param_dict)
@@ -33,7 +50,7 @@ for id in range(1, S+1):
     scip.setPresolve(SCIP_PARAMSETTING.OFF)
     scip.hideOutput()
     scip.optimize()
-    print_results(scip)
+    print_results(instancename, scip)
     if save_output:
         store_results(instancename, scip, "test_output.txt")
 
