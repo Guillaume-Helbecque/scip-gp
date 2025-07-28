c303c43885f0f4f136db3a0c81ed40ff492ae9eb
Guillaume-Helbecque <helbecque.guillaume@gmail.com>
2025-07-28 15:27:22 +0200

    move score computations in a dedicated function

--- branching/StrongMultiBranchingRule.py
+++ branching/StrongMultiBranchingRule.py
@@ -75,6 +75,54 @@ class StrongMultiBranchingRule(Branchrule):
 
         return child_down, None, child_up
 
+    def compute_score(self, var_set, s):
+        lpobjval = self.scip.getLPObjVal()
+
+        s_floor = math.floor(s)
+        s_ceil = math.ceil(s)
+
+        # ---- Down branch ----
+        self.scip.startProbing()
+        cons_down = self.scip.createConsFromExpr(quicksum(var_set) <= s_floor, local=True)
+        self.scip.addConsLocal(cons_down)
+        self.scip.constructLP()
+        self.scip.solveProbingLP(itlim=200)
+        down_inf = (self.scip.getLPSolstat() != SCIP_LPSOLSTAT.OPTIMAL)
+        bound_down = None if down_inf else self.scip.getLPObjVal()
+        self.scip.endProbing()
+
+        # ---- Up branch ----
+        self.scip.startProbing()
+        cons_up = self.scip.createConsFromExpr(quicksum(var_set) >= s_ceil, local=True)
+        self.scip.addConsLocal(cons_up)
+        self.scip.constructLP()
+        self.scip.solveProbingLP(itlim=200)
+        up_inf = (self.scip.getLPSolstat() != SCIP_LPSOLSTAT.OPTIMAL)
+        bound_up = None if up_inf else self.scip.getLPObjVal()
+        self.scip.endProbing()
+
+        if down_inf and up_inf:
+            return {"result": SCIP_RESULT.CUTOFF}
+
+        if not down_inf:
+            down_gain = max([bound_down - lpobjval, 0])
+        else:
+            down_gain = 0
+        if not up_inf:
+            up_gain = max([bound_up - lpobjval, 0])
+        else:
+            up_gain = 0
+
+        if debug: print(f"down inf = {down_inf}, up inf = {up_inf}")
+        if debug: print(f"bound_down = {bound_down}, bound_up = {bound_up}")
+        if debug: print(f"down gain = {down_gain}, up gain = {up_gain}")
+
+        score = max(down_gain, up_gain)
+        # score = self.scip.getBranchScoreMultiple(branch_cands[idx_set], [down_gain, up_gain])
+        if debug: print(f"score = {score}")
+
+        return score, bound_down, bound_up
+
     def branchexeclp(self, allowaddcons):
         # Get branching candidates
         branch_cands, branch_cand_sols, branch_cand_fracs, \
@@ -92,10 +140,8 @@ class StrongMultiBranchingRule(Branchrule):
         if debug: print("branch_cand_sols = ", branch_cand_sols)
         if debug: print("sense : ", self.scip.getObjectiveSense())
 
-        lpobjval = self.scip.getLPObjVal()
         if debug: print(self.scip.getConss())
 
-        if debug: print("lpobjval = ", lpobjval)
         if debug: print("Primal bound = ", self.scip.getPrimalbound())
         if debug: print("Dual bound = ", self.scip.getDualbound())
 
@@ -123,48 +169,7 @@ class StrongMultiBranchingRule(Branchrule):
             if abs(s - round(s)) < 1e-6:
                 continue
 
-            s_floor = math.floor(s)
-            s_ceil = math.ceil(s)
-
-            # ---- Down branch ----
-            self.scip.startProbing()
-            cons_down = self.scip.createConsFromExpr(quicksum(var_set) <= s_floor, local=True)
-            self.scip.addConsLocal(cons_down)
-            self.scip.constructLP()
-            self.scip.solveProbingLP(itlim=200)
-            down_inf = (self.scip.getLPSolstat() != SCIP_LPSOLSTAT.OPTIMAL)
-            bound_down = None if down_inf else self.scip.getLPObjVal()
-            self.scip.endProbing()
-
-            # ---- Up branch ----
-            self.scip.startProbing()
-            cons_up = self.scip.createConsFromExpr(quicksum(var_set) >= s_ceil, local=True)
-            self.scip.addConsLocal(cons_up)
-            self.scip.constructLP()
-            self.scip.solveProbingLP(itlim=200)
-            up_inf = (self.scip.getLPSolstat() != SCIP_LPSOLSTAT.OPTIMAL)
-            bound_up = None if up_inf else self.scip.getLPObjVal()
-            self.scip.endProbing()
-
-            if down_inf and up_inf:
-                return {"result": SCIP_RESULT.CUTOFF}
-
-            if not down_inf:
-                down_gain = max([bound_down - lpobjval, 0])
-            else:
-                down_gain = 0
-            if not up_inf:
-                up_gain = max([bound_up - lpobjval, 0])
-            else:
-                up_gain = 0
-
-            if debug: print(f"down inf = {down_inf}, up inf = {up_inf}")
-            if debug: print(f"bound_down = {bound_down}, bound_up = {bound_up}")
-            if debug: print(f"down gain = {down_gain}, up gain = {up_gain}")
-
-            score = max(down_gain, up_gain)
-            # score = self.scip.getBranchScoreMultiple(branch_cands[idx_set], [down_gain, up_gain])
-            if debug: print(f"score = {score}")
+            score, bound_down, bound_up = self.compute_score(var_set, s)
 
             if score > best_score:
                 best_score = score
